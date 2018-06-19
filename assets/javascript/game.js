bedf4e9f366d11fab756734e784856def112ab6d
Ciwonie <ciwonie@gmail.com>
2018-06-19 16:26:29 -0500

    updated javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,7 @@
 // 9. After the user wins/loses the game should automatically choose another word and make the user play it.
 
 //Computer will randomly select one of these words
-var randomWords = ['gandolf', 'yoshi', 'microsoft','stormtrooper', 'konichiwa', 'sevilla', 'pikachu'];
+var randomWords = ['gandolf', 'yoshi', 'microsoft', 'stormtrooper', 'konichiwa', 'sevilla', 'pikachu', 'bonobo','bailando','sprezzatura', 'supercalifragilisticexpialidocious'];
 
 //initial variables
 var wins = 0;
@@ -31,30 +31,60 @@ var guessedLetters = [];
 
 //generated variables
 
-var computerPick = Math.floor(Math.random() * randomWords.length);
-var answer = randomWords[computerPick]
-var length = answer.length;
-var display = [length];
-var win = length;
-var letters = answer.split('');
-var output = '';
-
-// var userLetter = ''; we're going to obtain this from the eventlistener
+var computerPick = Math.floor(Math.random() * randomWords.length); //computer selects an index from 'randomWords' array
+var answer = randomWords[computerPick] // variable 'answer' is then set to equal a word from 'randwords' array based on the selected index
+var answerLength = answer.length; //'answerlength' is a number based on the length of the chosen word
+var answerDisplay = []; //this is an empty array that will display our underscores in a future function
+var letters = answer.split(''); // this takes the word chosen from 'answer' and splits it into an array of divided characters
 
 
-reset(); //will add a reset function at bottom of page
+// this for loop will generate an array filled with underscores equal to the length of 'answer'
+for (var i = 0; i < answer.length; i++) {
+    answerDisplay[i] = '_';
+    console.log(answerDisplay);
+};
 
 //Initalizing a function from keypress. This begins the game.
-document.addEventListener('keypress', function(event) {
+document.addEventListener('keypress', function (event) {
 
     userGuess = event.key.toLowerCase();
+    console.log(userGuess);
 
-    for (var i = 0; i < answer.length; i++) {
-        display[i] = '_ ';
-        
+    // this is our win condition
+    if (answer === randomWords[10]) {
+        document.getElementById('hint').innerHTML = "Hint: this is a looooooooooooong word";
     }
 
+    if (letters.indexOf(userGuess) === -1) {
+        guessedLetters.push(userGuess);
+        guesses--;
+        console.log(guesses);
+    }
 
-    
+    for (var i = 0; i < answer.length; i++) {
+        if (userGuess === letters[i]) {
+            answerDisplay[i] = userGuess;
+        }
+    }
 
+    if (answerDisplay.join("") === answer) {
+        alert('you win');
+        wins++;
+    }
+
+    if (guesses === 0) {
+        losses++;
+        alert('you lose');
+    }
+
+    document.getElementById('blanks').innerHTML = "Fill in the blank: " + answerDisplay.join(" ");
+    document.getElementById('guesses').innerHTML = "Guesses: " + guessedLetters;
 });
+
+//Struggling with a reset function. I'd like something that resets the computerPick, guesses, and displays whenever a win/lose is recorded.
+// function reset () {
+//     computerPick = Math.floor(Math.random() * randomWords.length);
+//     answerDisplay = [];
+//     guessedLetters = [];
+//     guesses = 8;
+// };
